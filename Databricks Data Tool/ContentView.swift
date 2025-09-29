ea98cd0d47eac583dea884b24a989b1879cf81c9
Paul Thornton <paul.thornton@gmail.com>
2025-09-29 17:26:22 +1000

    Initial Commit

---
+++ Databricks Data Tool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Databricks Data Tool
+//
+//  Created by Paul Thornton on 29/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Databricks Data Tool/Databricks_Data_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Databricks_Data_ToolApp.swift
+//  Databricks Data Tool
+//
+//  Created by Paul Thornton on 29/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct Databricks_Data_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
